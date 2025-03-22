bbca7e89416593e616fc77448f191a4d315bfda2
Jazztine Cruz <me@jazztinecruz.com>
2025-03-22 20:03:55 +0800

    chore: tailwind config and install heroui and heroicons

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,22 @@
+const { heroui } = require("@heroui/react");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#000000",
+        secondary: "#FFFFFF",
+        accent: "#76522F",
+      },
+    },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [heroui()],
 };
